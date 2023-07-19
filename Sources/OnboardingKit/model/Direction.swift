7add78290f6cde1676f013fd63732d9968fc939b
Mauricio Silva <de.mauricio.silva@gmail.com>
2023-07-19 13:54:56 -0300

    Complete project(for now)

---
+++ Sources/OnboardingKit/model/Direction.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mauricio Silva on 19/07/23.
+//
+
+import Foundation
+
+enum Direction {
+    case left
+    case right
+}
---
+++ Sources/OnboardingKit/model/Slide.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mauricio Silva on 18/07/23.
+//
+
+import UIKit
+
+public class Slide {
+    
+    public let image: UIImage
+    public let title: String
+    
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+    
+    
+}
